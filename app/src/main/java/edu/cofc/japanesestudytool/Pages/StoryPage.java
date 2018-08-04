b540229a60bf5321d3740e2010c6aac9e37d0a0b
delacruzkb <delacruzkb@g.cofc.edu>
2018-08-04 18:57:40 -0400

    changed colors and added some StoryPage implementation

--- app/src/main/java/edu/cofc/japanesestudytool/Pages/StoryPage.java
+++ app/src/main/java/edu/cofc/japanesestudytool/Pages/StoryPage.java
@@ -1,14 +1,19 @@
 package edu.cofc.japanesestudytool.Pages;
 
 import edu.cofc.japanesestudytool.R;
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class StoryPage extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story_page);
+        Intent intent = getIntent();
+        String test = intent.getStringExtra("id");
     }
 }
