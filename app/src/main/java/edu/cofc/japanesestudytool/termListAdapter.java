0c957625afb02297806785d7c03572004ac03551
delacruzkb <delacruzkb@g.cofc.edu>
2018-08-09 21:50:52 -0400

    added termListAdapter

---
+++ app/src/main/java/edu/cofc/japanesestudytool/termListAdapter.java
@@ -0,0 +1,91 @@
+package edu.cofc.japanesestudytool;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+
+public class termListAdapter extends BaseAdapter
+{
+    private Context context;
+    private LayoutInflater mLayoutInflater;
+    private ArrayList<Term> data;
+    private boolean useKanji;
+    private boolean lessonKanjiOnly;
+    public termListAdapter(Context context, ArrayList<Term> data, boolean useKanji, boolean lessonKanjiOnly)
+    {
+        this.context = context;
+        this.data = data;
+        mLayoutInflater =(LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.useKanji = useKanji;
+        this.lessonKanjiOnly = lessonKanjiOnly;
+    }
+    @Override
+    public int getCount()
+    {
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int position)
+    {
+        return data.get(position);
+    }
+
+    @Override
+    public long getItemId(int position)
+    {
+        return position;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        View rowView = mLayoutInflater.inflate(R.layout.term_list_item, parent,false);
+        final Term term = (Term) getItem(position);
+        CheckBox checkBox = rowView.findViewById(R.id.checkBox);
+        final EditText termText = rowView.findViewById(R.id.termText);
+        Button toEng = rowView.findViewById(R.id.toEnglishButton);
+        toEng.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                termText.setText(term.getEng());
+            }
+        });
+        Button toJpns = rowView.findViewById(R.id.toJapaneseButton);
+        toJpns.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                termText.setText(term.getJpns());
+            }
+        });
+        Button toKanji = rowView.findViewById(R.id.toKanjiButton);
+        toKanji.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                termText.setText(term.getKanji());
+            }
+        });
+        if( useKanji)
+        {
+            termText.setText(term.getKanji());
+            if(lessonKanjiOnly && !term.getReqKanji())
+            {
+                toKanji.setVisibility(View.INVISIBLE);
+                termText.setText(term.getJpns());
+            }
+        }
+        else
+        {
+            termText.setText(term.getJpns());
+        }
+
+        return rowView;
+    }
+
+}
