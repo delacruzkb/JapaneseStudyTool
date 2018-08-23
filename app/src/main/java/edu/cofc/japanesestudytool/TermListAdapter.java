43fa64f493de97c139d4e687f17398d18265fd81
delacruzkb <delacruzkb@g.cofc.edu>
2018-08-23 15:36:51 -0400

    fixed typo

--- app/src/main/java/edu/cofc/japanesestudytool/TermListAdapter.java
+++ app/src/main/java/edu/cofc/japanesestudytool/TermListAdapter.java
@@ -75,7 +75,7 @@ public class TermListAdapter extends BaseAdapter
                 termText.setText(term.getKanji());
             }
         });
-        if( (term.getKanji()==null) || (term.getKanji().equalsIgnoreCase("NULL"))
+        if( (term.getKanji()==null) || (term.getKanji().equalsIgnoreCase("NULL")))
         {
             toKanji.setVisibility(View.INVISIBLE);
         }
