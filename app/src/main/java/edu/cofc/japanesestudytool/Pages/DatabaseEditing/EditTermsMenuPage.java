18871f15b4ebfa30c352d72a7fe11d00d42d9915
delacruzkb <delacruzkb@g.cofc.edu>
2019-02-19 23:33:35 -0500

    added a todo

--- app/src/main/java/edu/cofc/japanesestudytool/Pages/DatabaseEditing/EditTermsMenuPage.java
+++ app/src/main/java/edu/cofc/japanesestudytool/Pages/DatabaseEditing/EditTermsMenuPage.java
@@ -24,6 +24,7 @@ public class EditTermsMenuPage extends AppCompatActivity
     private Button searchButton;
     private Spinner dropDownBar,specificDropDownBar;
     private EditText searchEditTextBox;
+    //TODO: filter out grammar switch
     private Switch searchExactSwitch,beginWithSwitch;
     private final String[] items = new String[]{"Japanese","English","Kanji","Lesson"};
     private final String[] lessonSpec = new String[]{"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19",
